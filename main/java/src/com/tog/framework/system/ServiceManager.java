14c1b35e5c32b0ef7d4710e64a7c56adf4146fe5
Arrem <arrem.derplin@gmail.com>
2013-07-21 11:14:42 +0200

    Added a way to get a service by class.

--- main/java/src/com/tog/framework/system/ServiceManager.java
+++ main/java/src/com/tog/framework/system/ServiceManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class ServiceManager {
     private static final ArrayList<Service> services = new ArrayList<>();
+
     public static Service createService(Class<?> class_) {
         Service s;
         if ((s = getService(class_.getCanonicalName())) != null)
@@ -21,6 +22,14 @@ public class ServiceManager {
     public static Service getService(String name) {
         for (Service s : services) {
             if (s.getClass().getName().equals(name))
+                return s;
+        }
+        return null;
+    }
+
+    public static Service getService(Class<?> class_) {
+        for (Service s : services) {
+            if (s.getClass().equals(class_))
                 return s;
         }
         return null;
