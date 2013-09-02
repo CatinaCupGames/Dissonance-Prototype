62b50361a92682a1ee17d358c01d12d4d33003ea
Eddie Penta <eddie@gamezgalaxy.com>
2013-09-02 00:15:57 -0400

    Added Vec2 constructor for Position

--- main/java/src/com/dissonance/framework/game/ai/Position.java
+++ main/java/src/com/dissonance/framework/game/ai/Position.java
@@ -1,5 +1,7 @@
 package com.dissonance.framework.game.ai;
 
+import org.jbox2d.common.Vec2;
+
 import java.io.Serializable;
 
 public final class Position implements Serializable {
@@ -10,6 +12,10 @@ public final class Position implements Serializable {
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Position(Vec2 vector) {
+        this((int)vector.x, (int)vector.y);
     }
 
     public int getX() {
