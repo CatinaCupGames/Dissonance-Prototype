b16b8ba18ab736cea6e8e5addbddea461ec177ba
oyasunadev <oliver-fc@hotmail.com>
2013-09-01 21:24:09 -0400

    Added basic settings for gamepad controls

--- main/java/src/com/dissonance/framework/system/Settings.java
+++ main/java/src/com/dissonance/framework/system/Settings.java
@@ -1,5 +1,6 @@
 package com.dissonance.framework.system;
 
+import net.java.games.input.Component;
 import net.java.games.input.Controller;
 
 import java.util.HashMap;
@@ -16,4 +17,17 @@ public final class Settings
      * Whether or not a controller exists, and if so, is the user using a controller instead of the keyboard.
      */
     public static boolean usingController = false;
+
+	public static Component.Identifier.Axis move;
+	public static Component.Identifier.Button jump;
+	public static Component.Identifier.Button dodge;
+	public static Component.Identifier.Button cycleParty;
+	public static Component.Identifier.Button strafe;
+	public static Component.Identifier.Button attackSelect;
+	public static Component.Identifier.Button specialAbility;
+	public static Component.Identifier.Button magic1;
+	public static Component.Identifier.Button magic2;
+	public static Component.Identifier.Button equipmentStatusMagicMenu;
+	public static Component.Identifier.Button pause;
+	public static Component.Identifier.Axis extendedLook;
 }
