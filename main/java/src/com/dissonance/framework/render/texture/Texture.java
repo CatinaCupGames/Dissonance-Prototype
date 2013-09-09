05c1ded3dedae0a26706cfd570bf479ebfb8f270
Eddie Penta <eddie@gamezgalaxy.com>
2013-09-08 22:00:57 -0400

    Created TileTexture class for #28

--- main/java/src/com/dissonance/framework/render/texture/Texture.java
+++ main/java/src/com/dissonance/framework/render/texture/Texture.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 import static org.lwjgl.opengl.GL11.*;
 
-public class Texture {
+public abstract class Texture {
     int textureId;
     private int targetId;
     private int image_width, image_height;
---
+++ main/java/src/com/dissonance/framework/render/texture/tile/TileTexture.java
@@ -0,0 +1,15 @@
+package com.dissonance.framework.render.texture.tile;
+
+import com.dissonance.framework.render.texture.Texture;
+
+public class TileTexture extends Texture {
+
+
+    protected TileTexture(int i1, int i2) {
+        super(i1, i2);
+    }
+
+    protected TileTexture(Texture texture) {
+        super(texture);
+    }
+}
