2a3c0c50aef84762a7f2ec2646764bb2ecc9ddac
Lauren Clayberg <lauren.clayberg@gmail.com>
2014-01-14 14:36:37 -0500

    Removed test NPC

--- main/java/src/com/dissonance/game/w/test_tileset.java
+++ main/java/src/com/dissonance/game/w/test_tileset.java
@@ -18,12 +18,5 @@ public class test_tileset extends GameWorldLoader {
     @Override
     public void onLoad(World w) {
         super.onLoad(w);
-
-        TextureLoader.setFastRedraw(false);
-        TestNPC var1 = new TestNPC();
-        w.loadAndAdd(var1);
-        var1.setX(400f);
-        var1.setY(600f);
-        TextureLoader.setFastRedraw(true);
     }
 }
