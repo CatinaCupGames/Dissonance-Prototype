8abeb100e292624ec04066f086d8dc737f1e01f5
Eddie <eddie@gamezgalaxy.com>
2014-02-22 23:07:46 -0500

    ShieldSpell now implements the Spell class

--- main/java/src/com/dissonance/game/spells/ShieldSpell.java
+++ main/java/src/com/dissonance/game/spells/ShieldSpell.java
@@ -5,19 +5,28 @@ import com.dissonance.game.spells.statuseffects.HPHeal;
 import com.dissonance.game.spells.statuseffects.MPHeal;
 import com.dissonance.framework.game.sprites.impl.game.CombatSprite;
 
-public class ShieldSpell {
+public class ShieldSpell implements Spell {
     protected CombatSprite owner;
 
     public ShieldSpell(CombatSprite owner) {
         this.owner = owner;
     }
 
-    /*@Override
+    @Override
     public CombatSprite getOwner() {
         return owner;
     }
 
     @Override
-    public void castSpell() {
-    }*/
+    public String getName() {
+        return "ShieldSpell";
+    }
+
+    @Override
+    public void castSpell() { }
+
+    @Override
+    public int mpCost() {
+        return 0;
+    }
 }
