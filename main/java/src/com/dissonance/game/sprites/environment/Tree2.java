146bfaf565e67af81826705d5ba44789db3a986d
Eddie <eddie@gamezgalaxy.com>
2014-04-28 13:23:33 -0400

    Added environment.Tree1 and environment.Tree2

--- main/java/src/com/dissonance/game/sprites/environment/Tree2.java
+++ main/java/src/com/dissonance/game/sprites/environment/Tree2.java
@@ -2,8 +2,8 @@ package com.dissonance.game.sprites.environment;
 
 import com.dissonance.game.sprites.ImageSprite;
 
-public class Tree1 extends ImageSprite {
-    public Tree1() {
-        super("sprites/img/tree1.png");
+public class Tree2 extends ImageSprite {
+    public Tree2() {
+        super("sprites/img/tree2.png");
     }
 }
