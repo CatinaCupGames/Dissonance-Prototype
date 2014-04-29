823a5920ce3982341aafd5ce00070bf97e2caeb4
Eddie <eddie@gamezgalaxy.com>
2014-04-29 14:51:41 -0400

    Pretty much finished Outdoor world

--- main/java/src/com/dissonance/game/w/ToZesilia.java
+++ main/java/src/com/dissonance/game/w/ToZesilia.java
@@ -1,5 +1,6 @@
 package com.dissonance.game.w;
 
+import com.dissonance.framework.game.ai.astar.FastMath;
 import com.dissonance.framework.game.world.World;
 import com.dissonance.framework.game.world.WorldLoader;
 import com.dissonance.game.sprites.Farrand;
@@ -26,25 +27,44 @@ public class ToZesilia implements WorldLoader {
 
 
         final Random random = new Random();
-        /*for (int y = 7; y <= 20; y++) {
+        for (int y = 2; y <= 8; y++) {
             for (int x = 5; x <= 75; x++) {
                 int num = random.nextInt(2);
                 switch (num) {
                     case 0:
                         Tree1 tree = new Tree1();
                         w.loadAndAdd(tree);
-                        tree.setX(x * 16);
-                        tree.setY(y * 16);
+                        tree.setX(FastMath.fastCeil(x * 32f) + 5f);
+                        tree.setY(FastMath.fastCeil(y * 32f) + 5f);
                         break;
                     case 1:
                         Tree2 tree2 = new Tree2();
                         w.loadAndAdd(tree2);
-                        tree2.setX(x * 16);
-                        tree2.setY(y * 16);
+                        tree2.setX(FastMath.fastCeil(x * 32f) + 5f);
+                        tree2.setY(FastMath.fastCeil(y * 32f) + 5f);
                         break;
                 }
             }
-        }*/
+        }
+        for (int y = 15; y <= 18; y++) {
+            for (int x = 5; x <= 75; x++) {
+                int num = random.nextInt(2);
+                switch (num) {
+                    case 0:
+                        Tree1 tree = new Tree1();
+                        w.loadAndAdd(tree);
+                        tree.setX(x * 32);
+                        tree.setY(y * 32);
+                        break;
+                    case 1:
+                        Tree2 tree2 = new Tree2();
+                        w.loadAndAdd(tree2);
+                        tree2.setX(x * 32);
+                        tree2.setY(y * 32);
+                        break;
+                }
+            }
+        }
     }
 
     @Override
