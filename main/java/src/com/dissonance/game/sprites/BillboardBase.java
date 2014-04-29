99b5ef96fedeaea9225773d4dd9313b08f41f140
Eddie <eddie@gamezgalaxy.com>
2014-04-29 14:35:07 -0400

    Added code to BillboardBase class

--- main/java/src/com/dissonance/game/sprites/BillboardBase.java
+++ main/java/src/com/dissonance/game/sprites/BillboardBase.java
@@ -1,4 +1,17 @@
 package com.dissonance.game.sprites;
 
-public class BillboardBase {
+import com.dissonance.framework.game.sprites.impl.AnimatedSprite;
+
+public class BillboardBase extends AnimatedSprite {
+    @Override
+    public String getSpriteName() {
+        return "billboardbase";
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        setAnimation("test");
+        playAnimation();
+    }
 }
