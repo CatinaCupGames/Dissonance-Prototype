e0d42f802feacffec1796fa4fd8622a3be1d1392
Eddie <eddie@gamezgalaxy.com>
2014-05-02 11:40:50 -0400

    Enter button in WorldLoader now makes a new sprite

--- main/java/src/com/dissonance/game/w/Zesilia_pan.java
+++ main/java/src/com/dissonance/game/w/Zesilia_pan.java
@@ -2,11 +2,15 @@ package com.dissonance.game.w;
 
 import com.dissonance.framework.game.world.World;
 import com.dissonance.framework.game.world.WorldLoader;
+import com.dissonance.framework.render.shader.impl.Light;
 
 public class Zesilia_pan implements WorldLoader {
     @Override
     public void onLoad(World world) {
+        world.setWorldBrightness(0.6f);
 
+        world.createLight(0f, 0f, 1.1f, 1.f);
+        world.createLight(0f, 0f, 1.1f, 1.f);
     }
 
     @Override
