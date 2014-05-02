b68ab134a60c0e396700a30115550178677f735c
Henry Walker <henrywalker@gmail.com>
2014-05-02 11:41:05 -0400

    Scripted gate scene and entry scene
    also added dialog for extra escort scene

--- main/java/src/com/dissonance/game/scenes/GateScene.java
+++ main/java/src/com/dissonance/game/scenes/GateScene.java
@@ -1,6 +1,12 @@
 package com.dissonance.game.scenes;
 
+import com.dissonance.framework.game.ai.astar.Position;
+import com.dissonance.framework.game.ai.waypoint.WaypointType;
 import com.dissonance.framework.game.scene.SimpleScene;
+import com.dissonance.framework.game.scene.dialog.Dialog;
+import com.dissonance.framework.render.Camera;
+import com.dissonance.framework.render.RenderService;
+import com.dissonance.game.w.EntryPart1;
 
 /**
  * Created by Henry on 5/2/2014.
@@ -9,6 +15,36 @@ public class GateScene extends SimpleScene{
 
     @Override
     protected void playScene() throws Throwable {
-        
+        RenderService.INSTANCE.fadeFromBlack(1500);
+        RenderService.INSTANCE.waitForFade();
+
+        Camera.followSprite(EntryPart1.farrand);
+
+      EntryPart1.farrand.setWaypoint(
+              new Position(EntryPart1.farrand.getX() - 752,EntryPart1.farrand.getY()),
+              WaypointType.SIMPLE);
+
+        EntryPart1.jeremiah.setWaypoint(
+                new Position(EntryPart1.jeremiah.getX() - 752, EntryPart1.jeremiah.getY()),
+                WaypointType.SIMPLE);
+
+        Dialog.displayDialog("entering_zesilia1");
+
+        EntryPart1.farrand.waitForWaypointReached();
+
+
+
+        Camera.followSprite(EntryPart1.guard);
+
+        Dialog.displayDialog("entering_zesilia2");
+
+        //open he gates
+
+
+
+
+
+
+
     }
 }
--- main/java/src/com/dissonance/game/w/EntryPart1.java
+++ main/java/src/com/dissonance/game/w/EntryPart1.java
@@ -4,10 +4,12 @@ import com.dissonance.framework.game.world.World;
 import com.dissonance.framework.game.world.WorldLoader;
 import com.dissonance.game.sprites.Farrand;
 import com.dissonance.game.sprites.Jeremiah;
+import com.dissonance.game.sprites.Wyatt;
 
 public class EntryPart1 implements WorldLoader {
     public static Farrand farrand;
     public static Jeremiah jeremiah;
+    public static Wyatt guard; //change to guard
     @Override
     public void onLoad(World world) {
 
