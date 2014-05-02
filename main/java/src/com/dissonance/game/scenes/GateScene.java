a4236d74980d03e118df9108d3d8c1a8d565d3d0
Henry Walker <henrywalker@gmail.com>
2014-05-02 10:19:48 -0400

    small edits to dialog

---
+++ main/java/src/com/dissonance/game/scenes/GateScene.java
@@ -0,0 +1,14 @@
+package com.dissonance.game.scenes;
+
+import com.dissonance.framework.game.scene.SimpleScene;
+
+/**
+ * Created by Henry on 5/2/2014.
+ */
+public class GateScene extends SimpleScene{
+
+    @Override
+    protected void playScene() throws Throwable {
+        
+    }
+}
