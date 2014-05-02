fd53131d9673eb66da8764c32c6e9f2431962f6d
Eddie <eddie@gamezgalaxy.com>
2014-05-02 10:18:49 -0400

    started EntryPart1 world loader

---
+++ main/java/src/com/dissonance/game/w/EntryPart1.java
@@ -0,0 +1,20 @@
+package com.dissonance.game.w;
+
+import com.dissonance.framework.game.world.World;
+import com.dissonance.framework.game.world.WorldLoader;
+import com.dissonance.game.sprites.Farrand;
+import com.dissonance.game.sprites.Jeremiah;
+
+public class EntryPart1 implements WorldLoader {
+    public static Farrand farrand;
+    public static Jeremiah jeremiah;
+    @Override
+    public void onLoad(World world) {
+
+    }
+
+    @Override
+    public void onDisplay(World world) {
+
+    }
+}
