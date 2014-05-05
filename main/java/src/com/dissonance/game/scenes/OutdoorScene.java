dce6bf0138f83bf7e1f0cda1adc7bdcc2f512fa0
John Merrill <john.merrill95@gmail.com>
2014-05-05 19:04:19 -0400

    working on dialog fixes...

--- main/java/src/com/dissonance/game/scenes/OutdoorScene.java
+++ main/java/src/com/dissonance/game/scenes/OutdoorScene.java
@@ -3,6 +3,7 @@ package com.dissonance.game.scenes;
 import com.dissonance.framework.game.ai.astar.Position;
 import com.dissonance.framework.game.ai.waypoint.WaypointType;
 import com.dissonance.framework.game.scene.SimpleScene;
+import com.dissonance.framework.game.scene.dialog.Dialog;
 import com.dissonance.framework.render.Camera;
 import com.dissonance.framework.render.RenderService;
 import com.dissonance.game.w.ToZesilia;
@@ -13,17 +14,19 @@ public class OutdoorScene extends SimpleScene {
     protected void playScene() throws Throwable {
         RenderService.INSTANCE.fadeFromBlack(10000f);
         Vector2f center = Camera.translateToCameraCenter(ToZesilia.farrand.getVector(), ToZesilia.farrand.getHeight());
-        //center.x -= 330;
+        center.x -= 330;
         Camera.setPos(center);
-        center.x -= 400;
-        Camera.linearMovement(center, 7500);
+        center.x -= 2000;
+        Camera.linearMovement(center, 42500);
         Position farrandTarget = ToZesilia.farrand.getPosition();
         Position jeremiahTarget = ToZesilia.jeremiah.getPosition();
 
+
         ToZesilia.farrand.setMovementSpeed(8f);
-        ToZesilia.farrand.setWaypoint(new Position(farrandTarget.getX() - 600, farrandTarget.getY()), WaypointType.SIMPLE);
+        ToZesilia.farrand.setWaypoint(new Position(farrandTarget.getX() - 2600, farrandTarget.getY()), WaypointType.SIMPLE);
         ToZesilia.jeremiah.setMovementSpeed(8f);
-        ToZesilia.jeremiah.setWaypoint(new Position(jeremiahTarget.getX() - 700, jeremiahTarget.getY()), WaypointType.SIMPLE);
+        ToZesilia.jeremiah.setWaypoint(new Position(jeremiahTarget.getX() - 2700, jeremiahTarget.getY()), WaypointType.SIMPLE);
+        Dialog.displayDialog("Outdoor Scene");
         Camera.waitForEndOfMovement();
     }
 }
