d652403ef24e9b1190ff7c0926ff3df5e045fe00
AJ101095 <aj101095@gmail.com>
2014-05-05 14:23:13 -0400

    Rearranged Meeting
    I moved Waldemars meeting room around to match the new design and have
    been working on setting up my laptop now. Pretty much everything has
    needed to be checked so the steps have been slow and I haven't gotten
    very much done today.

--- main/java/src/com/dissonance/game/w/WaldemarsMeetingRoom.java
+++ main/java/src/com/dissonance/game/w/WaldemarsMeetingRoom.java
@@ -4,16 +4,7 @@
 This file was automatically generated with the
 World Loader Editor
 
-Date: 2014/05/05 11:54:05
-=====================================
-*/
-/*
-=====================================
-
-This file was automatically generated with the
-World Loader Editor
-
-Date: 2014/05/05 11:37:47
+Date: 2014/05/05 14:20:38
 =====================================
 */
 package com.dissonance.game.w;
@@ -21,63 +12,46 @@ package com.dissonance.game.w;
 import com.dissonance.framework.game.world.World;
 import com.dissonance.game.sprites.office.*;
 
+
 public class WaldemarsMeetingRoom extends GameWorldLoader {
-    public static DeskChair var10;
-    public static Sofa var9;
-    public static Sofa var8;
     public static Sofa var7;
-    public static PersonalDesk var6;
-    public static Bookshelf1 var5;
-    public static Bookshelf1 var4;
-    public static Bookshelf1 var3;
+    public static Sofa var6;
+    public static Sofa var5;
+    public static Bookshelf1 var1;
     public static Bookshelf1 var2;
+    public static Bookshelf2 var4;
     @Override
     public void onLoad(World w) {
         super.onLoad(w);
-    
+
+        var1 = new Bookshelf1();
+        w.loadAndAdd(var1);
+        var1.setX(220.44997f);
+        var1.setY(60f);
+
         var2 = new Bookshelf1();
         w.loadAndAdd(var2);
-        var2.setX(184.0f);
-        var2.setY(57.65f);
-    
-        var3 = new Bookshelf1();
-        w.loadAndAdd(var3);
-        var3.setX(235.04999f);
-        var3.setY(57.65f);
-    
-        var4 = new Bookshelf1();
+        var2.setX(323.49985f);
+        var2.setY(60.0f);
+
+        var4 = new Bookshelf2();
         w.loadAndAdd(var4);
-        var4.setX(285.15f);
-        var4.setY(57.65f);
+        var4.setX(271.85007f);
+        var4.setY(60.0f);
     
-        var5 = new Bookshelf1();
+        var5 = new Sofa();
         w.loadAndAdd(var5);
-        var5.setX(134.69998f);
-        var5.setY(57.65f);
+        var5.setX(374.40015f);
+        var5.setY(69.350006f);
     
-        var6 = new PersonalDesk();
+        var6 = new Sofa();
         w.loadAndAdd(var6);
-        var6.setX(132.35002f);
-        var6.setY(203.8f);
+        var6.setX(396.70007f);
+        var6.setY(69.350006f);
     
         var7 = new Sofa();
         w.loadAndAdd(var7);
-        var7.setX(52.949997f);
-        var7.setY(72.0f);
-    
-        var8 = new Sofa();
-        w.loadAndAdd(var8);
-        var8.setX(77.35f);
-        var8.setY(72.05f);
-    
-        var9 = new Sofa();
-        w.loadAndAdd(var9);
-        var9.setX(28.599997f);
-        var9.setY(72.05f);
-    
-        var10 = new DeskChair();
-        w.loadAndAdd(var10);
-        var10.setX(103.850006f);
-        var10.setY(184.1f);
+        var7.setX(418.80014f);
+        var7.setY(69.350006f);
     }
 }
