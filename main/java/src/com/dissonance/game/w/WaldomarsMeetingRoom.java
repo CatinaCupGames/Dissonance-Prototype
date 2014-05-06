0b062900ba35a4c6b22ca58da4deac22eb3cee6d
John Merrill <john.merrill95@gmail.com>
2014-05-06 15:06:20 -0400

    fixed transparency issues, progress on office scene

--- main/java/src/com/dissonance/game/w/WaldomarsMeetingRoom.java
+++ main/java/src/com/dissonance/game/w/WaldomarsMeetingRoom.java
@@ -15,7 +15,8 @@ import com.dissonance.game.sprites.office.*;
 
 
 public class WaldomarsMeetingRoom extends GameWorldLoader {
-    public static Waldomar waldomar;    
+    public static Waldomar waldomar;
+    public static Jeremiah jeremiah;
     
     public static WaldomarDesk var8;
     public static Sofa var7;
@@ -24,6 +25,14 @@ public class WaldomarsMeetingRoom extends GameWorldLoader {
     public static Bookshelf1 var1;
     public static Bookshelf1 var2;
     public static Bookshelf2 var4;
+    public static BlackCover var9;
+
+    public static Wyatt guard1;
+    public static Wyatt guard2;
+    public static Wyatt guard3;
+    public static Wyatt guard4;
+    public static Wyatt guard5;
+
     @Override
     public void onLoad(World w) {
         super.onLoad(w);
@@ -63,6 +72,12 @@ public class WaldomarsMeetingRoom extends GameWorldLoader {
         w.loadAndAdd(var8);
         var8.setX(113.5f);
         var8.setY(202.8f);
+
+        var9 = new BlackCover();
+        w.loadAndAdd(var9);
+        var9.setX(600f);
+        var9.setY(150f);
+        var9.setLayer(3);
     
         waldomar = new Waldomar();
         w.loadAndAdd(waldomar);
@@ -70,8 +85,42 @@ public class WaldomarsMeetingRoom extends GameWorldLoader {
         waldomar.setY(203.85004f);
 
         farrand.deselect();
-        farrand.setX(403.85f);
+        farrand.setX(453.85f);
         farrand.setY(120.95f);
 
+        jeremiah = new Jeremiah();
+        w.loadAndAdd(jeremiah);
+        jeremiah.setX(503f);
+        jeremiah.setY(150f);
+
+        guard1 = new Wyatt();
+        w.loadAndAdd(guard1);
+        guard1.setX(503f);
+        guard1.setY(150f);
+
+        guard2 = new Wyatt();
+        w.loadAndAdd(guard1);
+        guard2.setX(503f);
+        guard2.setY(150f);
+
+        guard3 = new Wyatt();
+        w.loadAndAdd(guard1);
+        guard3.setX(503f);
+        guard3.setY(150f);
+
+        guard3 = new Wyatt();
+        w.loadAndAdd(guard1);
+        guard3.setX(503f);
+        guard3.setY(150f);
+
+        guard4 = new Wyatt();
+        w.loadAndAdd(guard1);
+        guard4.setX(503f);
+        guard4.setY(150f);
+
+        guard5 = new Wyatt();
+        w.loadAndAdd(guard1);
+        guard5.setX(503f);
+        guard5.setY(150f);
     }
 }
