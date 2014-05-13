20ed5f8a6fe41daf75b1a359d44fb500f2c64857
Henry Walker <henrywalker@gmail.com>
2014-05-13 14:56:58 -0400

    Finished the first office floor

--- main/java/src/com/dissonance/game/w/OfficeFloor1.java
+++ main/java/src/com/dissonance/game/w/OfficeFloor1.java
@@ -4,245 +4,108 @@
 This file was automatically generated with the
 World Loader Editor
 
+Date: 2014/05/13 14:56:39
+=====================================
+*/
+
+/*
+=====================================
+
+This file was automatically generated with the
+World Loader Editor
+
+Date: 2014/05/13 14:24:16
+=====================================
+*/
+/*
+=====================================
+
+This file was automatically generated with the
+World Loader Editor
+
+Date: 2014/05/13 11:28:05
+=====================================
+*/
+
+/*
+=====================================
+
+This file was automatically generated with the
+World Loader Editor
+
 Date: 2014/05/09 14:59:35
 =====================================
 */
 package com.dissonance.game.w;
 
 import com.dissonance.framework.game.world.World;
-import com.dissonance.game.sprites.office.Cubicle;
+import com.dissonance.game.sprites.office.*;
 
 public class OfficeFloor1 extends GameWorldLoader {
-    public static Cubicle var1;
-    public static Cubicle var2;
-    public static Cubicle var3;
-    public static Cubicle var4;
-    public static Cubicle var5;
-    public static Cubicle var6;
-    public static Cubicle var7;
-    public static Cubicle var8;
-    public static Cubicle var9;
-    public static Cubicle var10;
-    public static Cubicle var11;
-    public static Cubicle var12;
-    public static Cubicle var13;
-    public static Cubicle var14;
-    public static Cubicle var15;
-    public static Cubicle var16;
-    public static Cubicle var17;
-    public static Cubicle var18;
-    public static Cubicle var19;
-    public static Cubicle var20;
-    public static Cubicle var21;
-    public static Cubicle var22;
-    public static Cubicle var23;
-    public static Cubicle var24;
-    public static Cubicle var25;
-    public static Cubicle var26;
-    public static Cubicle var27;
-    public static Cubicle var28;
-    public static Cubicle var29;
-    public static Cubicle var30;
-    public static Cubicle var31;
-    public static Cubicle var32;
-    public static Cubicle var33;
-    public static Cubicle var34;
-    public static Cubicle var35;
-    public static Cubicle var36;
-    public static Cubicle var37;
-    public static Cubicle var38;
+   
+    public static PottedPlant var8;
+    public static PottedPlant var9;
+    public static PottedPlant var10;
+    public static PottedPlant var11;
+
     @Override
-    public void onLoad(World w) {
+    public void onLoad(World w){
         super.onLoad(w);
 
-        var1 = new Cubicle();
-        w.loadAndAdd(var1);
-        var1.setX(101.19998f);
-        var1.setY(163.24988f);
+for(int i=1; i<7; i++){
+    for(int z=1; z<11; z++){
+if (i == 5 && z == 1) continue;
+        if (i == 6 && z == 1) continue;
+      Cubicle var = new Cubicle();
+        w.loadAndAdd(var);
+        var.setX(i*143-40f);
+        var.setY(40+z*125f);
+      }  
+     }  
 
-        var2 = new Cubicle();
-        w.loadAndAdd(var2);
-        var2.setX(101.19998f);
-        var2.setY(302.9497f);
 
-        var3 = new Cubicle();
-        w.loadAndAdd(var3);
-        var3.setX(101.19998f);
-        var3.setY(434.94962f);
+for(int i=1; i<7; i++){
+    for(int z=1; z<11; z++){
+if (i == 5 && z == 1) continue;
+ if (i == 6 && z == 1) continue;
+      tablecropped har = new tablecropped();
+        w.loadAndAdd(har);
+        har.setX(i*143-16f);
+        har.setY(30+z*125f);
+      }  
+     }  
 
-        var4 = new Cubicle();
-        w.loadAndAdd(var4);
-        var4.setX(101.19998f);
-        var4.setY(570.3495f);
+for(int i=1; i<7; i++){
+    for(int z=1; z<11; z++){
+ if (i == 6 && z == 1) continue;
+if (i == 5 && z == 1) continue;
+      DeskChair rar = new DeskChair();
+        w.loadAndAdd(rar);
+        rar.setX(i*143-38f);
+        rar.setY(24+z*125f);
+      }  
+     }  
 
-        var5 = new Cubicle();
-        w.loadAndAdd(var5);
-        var5.setX(101.19998f);
-        var5.setY(697.99963f);
 
-        var6 = new Cubicle();
-        w.loadAndAdd(var6);
-        var6.setX(101.19998f);
-        var6.setY(827.1996f);
-
-        var7 = new Cubicle();
-        w.loadAndAdd(var7);
-        var7.setX(101.19998f);
-        var7.setY(964.8998f);
-
-        var8 = new Cubicle();
+    var8 = new PottedPlant();
         w.loadAndAdd(var8);
-        var8.setX(101.19998f);
-        var8.setY(1092.2f);
+        var8.setX(850.3498f);
+        var8.setY(1395.44995f);
 
-        var9 = new Cubicle();
+    var9 = new PottedPlant();
         w.loadAndAdd(var9);
-        var9.setX(101.19998f);
-        var9.setY(1221.1501f);
+        var9.setX(850.3498f);
+        var9.setY(90.44995f);
 
-        var10 = new Cubicle();
+    var10 = new PottedPlant();
         w.loadAndAdd(var10);
-        var10.setX(101.19998f);
-        var10.setY(1352.2502f);
+        var10.setX(15.3498f);
+        var10.setY(1400.44995f);
 
-        var11 = new Cubicle();
+    var11 = new PottedPlant();
         w.loadAndAdd(var11);
-        var11.setX(291.44998f);
-        var11.setY(1353.1503f);
+        var11.setX(15.3498f);
+        var11.setY(90.44995f);
 
-        var12 = new Cubicle();
-        w.loadAndAdd(var12);
-        var12.setX(291.44998f);
-        var12.setY(1222.1006f);
-
-        var13 = new Cubicle();
-        w.loadAndAdd(var13);
-        var13.setX(291.44998f);
-        var13.setY(1092.551f);
-
-        var14 = new Cubicle();
-        w.loadAndAdd(var14);
-        var14.setX(291.44998f);
-        var14.setY(964.30096f);
-
-        var15 = new Cubicle();
-        w.loadAndAdd(var15);
-        var15.setX(291.44998f);
-        var15.setY(828.75085f);
-
-        var16 = new Cubicle();
-        w.loadAndAdd(var16);
-        var16.setX(291.44998f);
-        var16.setY(699.55084f);
-
-        var17 = new Cubicle();
-        w.loadAndAdd(var17);
-        var17.setX(291.44998f);
-        var17.setY(570.401f);
-
-        var18 = new Cubicle();
-        w.loadAndAdd(var18);
-        var18.setX(291.44998f);
-        var18.setY(436.60107f);
-
-        var19 = new Cubicle();
-        w.loadAndAdd(var19);
-        var19.setX(291.44998f);
-        var19.setY(300.45105f);
-
-        var20 = new Cubicle();
-        w.loadAndAdd(var20);
-        var20.setX(443.04993f);
-        var20.setY(300.05096f);
-
-        var21 = new Cubicle();
-        w.loadAndAdd(var21);
-        var21.setX(441.09995f);
-        var21.setY(435.05093f);
-
-        var22 = new Cubicle();
-        w.loadAndAdd(var22);
-        var22.setX(441.09995f);
-        var22.setY(570.151f);
-
-        var23 = new Cubicle();
-        w.loadAndAdd(var23);
-        var23.setX(441.09995f);
-        var23.setY(699.7008f);
-
-        var24 = new Cubicle();
-        w.loadAndAdd(var24);
-        var24.setX(441.09995f);
-        var24.setY(826.3508f);
-
-        var25 = new Cubicle();
-        w.loadAndAdd(var25);
-        var25.setX(441.09995f);
-        var25.setY(966.2009f);
-
-        var26 = new Cubicle();
-        w.loadAndAdd(var26);
-        var26.setX(441.09995f);
-        var26.setY(1094.8512f);
-
-        var27 = new Cubicle();
-        w.loadAndAdd(var27);
-        var27.setX(441.09995f);
-        var27.setY(1223.5015f);
-
-        var28 = new Cubicle();
-        w.loadAndAdd(var28);
-        var28.setX(441.09995f);
-        var28.setY(1356.201f);
-
-        var29 = new Cubicle();
-        w.loadAndAdd(var29);
-        var29.setX(612.99963f);
-        var29.setY(1356.151f);
-
-        var30 = new Cubicle();
-        w.loadAndAdd(var30);
-        var30.setX(612.99963f);
-        var30.setY(1224.251f);
-
-        var31 = new Cubicle();
-        w.loadAndAdd(var31);
-        var31.setX(612.99963f);
-        var31.setY(1098.901f);
-
-        var32 = new Cubicle();
-        w.loadAndAdd(var32);
-        var32.setX(612.99963f);
-        var32.setY(967.90106f);
-
-        var33 = new Cubicle();
-        w.loadAndAdd(var33);
-        var33.setX(612.99963f);
-        var33.setY(823.901f);
-
-        var34 = new Cubicle();
-        w.loadAndAdd(var34);
-        var34.setX(612.99963f);
-        var34.setY(701.75104f);
-
-        var35 = new Cubicle();
-        w.loadAndAdd(var35);
-        var35.setX(612.99963f);
-        var35.setY(572.4509f);
-
-        var36 = new Cubicle();
-        w.loadAndAdd(var36);
-        var36.setX(612.99963f);
-        var36.setY(444.051f);
-
-        var37 = new Cubicle();
-        w.loadAndAdd(var37);
-        var37.setX(612.99963f);
-        var37.setY(318.351f);
-
-        var38 = new Cubicle();
-        w.loadAndAdd(var38);
-        var38.setX(589.94977f);
-        var38.setY(170.85094f);
     }
 }
