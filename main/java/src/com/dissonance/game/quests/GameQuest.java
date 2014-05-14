245a820ad1942f5a79d8a3f1d0b6e044dc4a2eb2
Eddie <eddie@gamezgalaxy.com>
2014-05-14 13:33:06 -0400

    Lets make the actual game now

---
+++ main/java/src/com/dissonance/game/quests/GameQuest.java
@@ -0,0 +1,15 @@
+package com.dissonance.game.quests;
+
+import com.dissonance.framework.game.AbstractQuest;
+
+public class GameQuest extends AbstractQuest {
+    @Override
+    public void startQuest() throws Exception {
+
+    }
+
+    @Override
+    public String getName() {
+        return "the actual game";
+    }
+}
