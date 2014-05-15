63277e1b373de59da1116d55f72fc17b78b0f2f3
Henry Walker <henrywalker@gmail.com>
2014-05-15 11:32:29 -0400

    Started scripting atwaldomarscene
    also made the map for it

---
+++ main/java/src/com/dissonance/game/scenes/AtWaldomarScene.java
@@ -0,0 +1,30 @@
+package com.dissonance.game.scenes;
+
+import com.dissonance.framework.game.ai.waypoint.WaypointType;
+import com.dissonance.framework.game.scene.SimpleScene;
+import com.dissonance.framework.render.Camera;
+import com.dissonance.framework.render.RenderService;
+import com.dissonance.game.w.WaldoHallway;
+import com.dissonance.game.w.WaldomarsMeetingRoom;
+import org.lwjgl.util.vector.Vector2f;
+
+
+
+/**
+ * Created by Henry on 5/15/2014.
+ */
+public class AtWaldomarScene extends SimpleScene {
+
+
+    @Override
+    protected void playScene() throws Throwable {
+        RenderService.INSTANCE.fadeFromBlack(3000);
+        Vector2f center = Camera.translateToCameraCenter(WaldoHallway.farrand.getVector(), WaldoHallway.farrand.getHeight());
+        Camera.setPos(center);
+        Camera.followSprite(WaldoHallway.farrand);
+
+       // WaldomarsMeetingRoom.farrand.setWaypoint(0f, AtWaldomar.farrand.getY(), WaypointType.SIMPLE);
+        WaldomarsMeetingRoom.farrand.setMovementSpeed(3f);
+
+    }
+}
