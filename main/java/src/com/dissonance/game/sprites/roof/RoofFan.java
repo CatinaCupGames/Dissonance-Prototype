85468f4f037c3d18a523a511605c55e0116e68cb
AJ <AJ101095@aol.com>
2014-05-16 14:08:43 -0500

    Sprites
    I added the needed class files for all the roof sprites we have so far
    so I can use them in the world loader.

---
+++ main/java/src/com/dissonance/game/sprites/roof/RoofFan.java
@@ -0,0 +1,7 @@
+package com.dissonance.game.sprites.roof;
+
+import com.dissonance.game.sprites.ImageSprite;
+
+public class RoofFan extends ImageSprite {
+    public RoofFan(){super("sprites/img/rooffan.png");}
+}
---
+++ main/java/src/com/dissonance/game/sprites/roof/RoofVent.java
@@ -0,0 +1,7 @@
+package com.dissonance.game.sprites.roof;
+
+import com.dissonance.game.sprites.ImageSprite;
+
+public class RoofVent extends ImageSprite {
+    public RoofVent(){super("sprites/img/roofvent.png");}
+}
