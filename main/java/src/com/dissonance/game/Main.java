fb74cb4cfb97ccfae03228c1f5e8354da420856c
Henry Walker <henrywalker@gmail.com>
2014-05-16 11:07:53 -0400

    expanded the atwaldomap

--- main/java/src/com/dissonance/game/Main.java
+++ main/java/src/com/dissonance/game/Main.java
@@ -2,6 +2,7 @@ package com.dissonance.game;
 
 import com.dissonance.framework.game.GameService;
 import com.dissonance.game.quests.GateQuest;
+import com.dissonance.game.quests.Gate_2Quest;
 
 public class Main {
     public static String DID;
@@ -10,8 +11,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         GameService.loadEssentials(args);
 
-
-        GameService.beginQuest(new GateQuest());
+        GameService.beginQuest(new Gate_2Quest());
+        //GameService.beginQuest(new GateQuest());
         //GameService.beginQuest(new OfficeQuest());
         //GameService.beginQuest(new HallwayQuest());
     }
