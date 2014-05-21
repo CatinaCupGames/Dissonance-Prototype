dc2b9773e59a93b679c724bfaeb67363a42d905d
John Merrill <john.merrill95@gmail.com>
2014-05-21 02:30:50 -0400

    added a factory table

---
+++ main/java/src/com/dissonance/game/sprites/factory/FactoryTable1.java
@@ -0,0 +1,17 @@
+package com.dissonance.game.sprites.factory;
+
+import com.dissonance.game.sprites.ImagePhysicsSprite;
+
+/**
+ * Created by Jmerrill on 5/21/2014.
+ */
+public class FactoryTable1 extends ImagePhysicsSprite {
+    public FactoryTable1() {
+        super("sprites/img/FactoryTable1.png");
+    }
+
+    @Override
+    public String hitboxConfigPath() {
+        return null;
+    }
+}
