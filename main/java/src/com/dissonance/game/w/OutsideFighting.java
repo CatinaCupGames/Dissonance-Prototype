d9969467302cc143d9bb8c1c51784e5b6b68e3b2
John Merrill <john.merrill95@gmail.com>
2014-05-21 14:34:43 -0400

    dfkjhdfskjdfsh

--- main/java/src/com/dissonance/game/w/OutsideFighting.java
+++ main/java/src/com/dissonance/game/w/OutsideFighting.java
@@ -4,15 +4,21 @@ import com.dissonance.framework.game.world.World;
 import com.dissonance.game.sprites.StreetLamp;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class OutsideFighting extends DemoLevelWorldLoader {
+    int pleaseKillMe = 0;
+    int pleaseKillMeAgain = 0;
+
+    public static ArrayList<StreetLamp> lamps1 = new ArrayList<StreetLamp>();
+    public static ArrayList<StreetLamp> lamps2 = new ArrayList<StreetLamp>();
+
 
     @Override
     public void onLoad(World w) {
         super.onLoad(w);
 
-        farrand.setX(22*16);
-        farrand.setY(300*16);
+
 
         w.setWorldBrightness(0.96f);
         //DIS CUTOFF BULLSHIIIEEET
@@ -20,46 +26,61 @@ public class OutsideFighting extends DemoLevelWorldLoader {
 
         for(int y = 350; y>= 240; y -= 10){
             if((y / 10) % 2 == 0){
-                StreetLamp lamp = new StreetLamp();
-                w.loadAndAdd(lamp);
-                lamp.setX(26*16);
-                lamp.setY(y * 16);
-                lamp.setLayer(11);
-                lamp.setCutOffMargin(30f);
+                lamps1.add(new StreetLamp());
+                w.loadAndAdd(lamps1.get(pleaseKillMe));
+                lamps1.get(pleaseKillMe).setX(26*16);
+                lamps1.get(pleaseKillMe).setY(y * 16);
+                lamps1.get(pleaseKillMe).setLayer(1);
                 w.createLight(26* 16, (y - 3.5f)* 16, 1.2f, 0.03f, Color.YELLOW);
                 w.createLight(26* 16, (y - 3.5f)* 16, 1.4f, 0.73f);
+                pleaseKillMe ++;
             }else{
-                StreetLamp lamp = new StreetLamp();
-                w.loadAndAdd(lamp);
-                lamp.setX(35*16);
-                lamp.setY(y * 16);
-                lamp.setLayer(11);
-                lamp.setCutOffMargin(30f);
+                lamps1.add(new StreetLamp());
+                w.loadAndAdd(lamps1.get(pleaseKillMe));
+                lamps1.get(pleaseKillMe).setX(35 * 16);
+                lamps1.get(pleaseKillMe).setY(y * 16);
+                lamps1.get(pleaseKillMe).setLayer(1);
+
                 w.createLight(35* 16, (y - 3.5f)* 16, 1.2f, 0.03f, Color.YELLOW);
                 w.createLight(35* 16, (y - 3.5f)* 16, 1.4f, 0.73f);
+                pleaseKillMe ++;
             }
         }
 
         for(int y = 350; y>= 240; y -= 10){
             if((y / 10) % 2 == 0){
-                StreetLamp lamp = new StreetLamp();
-                w.loadAndAdd(lamp);
-                lamp.setX(121*16);
-                lamp.setY(y * 16);
-                lamp.setLayer(11);
-                //lamp.setCutOffMargin(30f);
+                lamps2.add(new StreetLamp());
+                w.loadAndAdd(lamps2.get(pleaseKillMeAgain));
+                lamps2.get(pleaseKillMeAgain).setX(121 * 16);
+                lamps2.get(pleaseKillMeAgain).setY(y * 16);
+                lamps2.get(pleaseKillMeAgain).setLayer(1);
                 w.createLight(121* 16, (y - 3.5f)* 16, 1.2f, 0.03f, Color.YELLOW);
                 w.createLight(121* 16, (y - 3.5f)* 16, 1.4f, 0.73f);
+                pleaseKillMeAgain ++;
             }else{
-                StreetLamp lamp = new StreetLamp();
-                w.loadAndAdd(lamp);
-                lamp.setX(130*16);
-                lamp.setY(y * 16);
-                lamp.setLayer(11);
-                //lamp.setCutOffMargin(33f);
-                w.createLight(130* 16, (y - 3.5f)* 16, 1.2f, 0.03f, Color.YELLOW);
+                lamps2.add(new StreetLamp());
+                w.loadAndAdd(lamps2.get(pleaseKillMeAgain));
+                lamps2.get(pleaseKillMeAgain).setX(130 * 16);
+                lamps2.get(pleaseKillMeAgain).setY(y * 16);
+                lamps2.get(pleaseKillMeAgain).setLayer(1);
+                w.createLight(130 * 16, (y - 3.5f) * 16, 1.2f, 0.03f, Color.YELLOW);
                 w.createLight(130* 16, (y - 3.5f)* 16, 1.4f, 0.73f);
+                pleaseKillMeAgain ++;
             }
         }
+
+
+    }
+    @Override
+    public void onDisplay(World w){
+        super.onDisplay(w);
+        farrand.setX(22*16);
+        farrand.setY(300*16);
+
+        for(int i = 0; i <= 23; i++){
+            lamps1.get(i).setCutOffMargin(22f);
+            lamps2.get(i).setCutOffMargin(22f);
+        }
+
     }
 }
