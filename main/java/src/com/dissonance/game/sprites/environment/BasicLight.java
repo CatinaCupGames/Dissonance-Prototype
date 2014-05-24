d49b4a30148b78f7c47d8e5ddef01fbeef48c930
Eddie <ecp4224@gmail.com>
2014-05-24 13:47:01 -0400

    Added quality light option for low-end computers

---
+++ main/java/src/com/dissonance/game/sprites/environment/BasicLight.java
@@ -0,0 +1,39 @@
+package com.dissonance.game.sprites.environment;
+
+import com.dissonance.framework.game.sprites.Sprite;
+import com.dissonance.framework.game.sprites.ui.UI;
+import com.dissonance.framework.render.Camera;
+import com.dissonance.framework.render.RenderService;
+import com.dissonance.game.sprites.ImageSprite;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class BasicLight extends ImageSprite implements UI {
+    public BasicLight() {
+        super("sprites/img/lightglow.png");
+    }
+
+    @Override
+    public void render() {
+        glPushMatrix();
+        glTranslatef(-Camera.getX(), -Camera.getY(), 0f);
+        glColor4f(1f, 1f, 1f, RenderService.getCurrentAlphaValue());
+
+        super.render();
+
+        glPopMatrix();
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        setLayer(100);
+    }
+
+    @Override
+    public void init() { }
+
+    @Override
+    public void update() { }
+}
--- main/java/src/com/dissonance/game/w/FactoryFloorCat.java
+++ main/java/src/com/dissonance/game/w/FactoryFloorCat.java
@@ -1,6 +1,8 @@
 package com.dissonance.game.w;
 
 import com.dissonance.framework.game.world.World;
+import com.dissonance.framework.system.GameSettings;
+import com.dissonance.game.sprites.environment.BasicLight;
 
 public class FactoryFloorCat extends DemoLevelWorldLoader {
     /*
@@ -25,24 +27,53 @@ public class FactoryFloorCat extends DemoLevelWorldLoader {
         jeremiah.setLayer(2);
         jeremiah.setVisible(false);
 
-        w.createLight(25*16, 199*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(40*16, 161*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(20*16, 161*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(24*16, 138*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(40*16, 138*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(24*16, 122*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(39*16, 112*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(24*16, 112*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(12*16, 112*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(34*16, 81*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(11*16, 81*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(8*16, 57*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(16*16, 32*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(30*16, 32*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(43*16, 32*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
-        w.createLight(54*16, 24*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+        if (GameSettings.Graphics.qualityLights) {
+            w.createLight(25*16, 199*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(40*16, 161*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(20*16, 161*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(24*16, 138*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(40*16, 138*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(24*16, 122*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(39*16, 112*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(24*16, 112*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(12*16, 112*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(34*16, 81*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(11*16, 81*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(8*16, 57*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(16*16, 32*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(30*16, 32*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(43*16, 32*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+            w.createLight(54*16, 24*16, WALL_LIGHT_BRIGHTNESS, 0.5f);
+        } else {
+            w.setWorldBrightness(0.4f);
+            w.createLight(-200, -200, 0.4f, 0.1f); //Lights for brightness
 
+            createLight(25*16, 199*16, w);
+            createLight(40*16, 161*16, w);
+            createLight(20*16, 161*16, w);
+            createLight(24*16, 138*16, w);
+            createLight(40*16, 138*16, w);
+            createLight(24*16, 122*16, w);
+            createLight(39*16, 112*16, w);
+            createLight(24*16, 112*16, w);
+            createLight(12*16, 112*16, w);
+            createLight(34*16, 81*16,  w);
+            createLight(11*16, 81*16,  w);
+            createLight(8*16, 57*16,   w);
+            createLight(16*16, 32*16,  w);
+            createLight(30*16, 32*16,  w);
+            createLight(43*16, 32*16,  w);
+            createLight(54*16, 24*16,  w);
+        }
+    }
 
-
+    private void createLight(float x, float y, World w) {
+        BasicLight light = new BasicLight();
+        light.setX(x);
+        light.setY(y);
+        light.setStartHeight(256f);
+        light.setStartWidth(256f);
+        light.setAlpha(0.4f);
+        w.loadAndAdd(light);
     }
 }
