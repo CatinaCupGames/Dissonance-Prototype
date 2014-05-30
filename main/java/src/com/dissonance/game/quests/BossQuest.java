995285e4604f7614d9e94210d1cc191be159fe3b
Eddie <ecp4224@gmail.com>
2014-05-30 13:35:56 -0400

    More work on the boss quest

--- main/java/src/com/dissonance/game/quests/BossQuest.java
+++ main/java/src/com/dissonance/game/quests/BossQuest.java
@@ -2,17 +2,23 @@ package com.dissonance.game.quests;
 
 import com.dissonance.framework.game.world.World;
 import com.dissonance.framework.game.world.WorldFactory;
+import com.dissonance.framework.render.Camera;
+import com.dissonance.game.scenes.BossStart;
+import com.dissonance.game.w.CityEntrySquare;
 import com.dissonance.game.w.CityEntrySquare2;
 
 public class BossQuest extends PauseQuest {
     @Override
     public void startQuest() throws Exception {
+        Camera.stopFollowing();
         WorldFactory.clearCache();
-        World world = WorldFactory.getWorld("CityEntrySquare", new CityEntrySquare2());
+        World world = WorldFactory.getWorld("CityEntrySquare2");
         setWorld(world);
         world.waitForWorldDisplayed();
+        CityEntrySquare2.farrand.freeze(true, BossQuest.class);
+        CityEntrySquare2.jeremiah.freeze(true, BossQuest.class);
 
-
+        playSceneAndWait(BossStart.class);
     }
 
     @Override
